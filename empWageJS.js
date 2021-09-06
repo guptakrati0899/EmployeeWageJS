d0858f5d2b107d121aa9f5a2c0aae365fae80f86
KratiGupta <kratigupta8545@gmail.com>
2021-09-06 09:10:03 +0530

    Added program to calculate conditional wage

--- empWageJS.js
+++ empWageJS.js
@@ -1,17 +1,18 @@
 const partTime = 1;
-const FullTime = 2;
-const PartTimeHours = 4;
-const FullTimeHours = 8;
-const WagePerHour = 20;
-const WorkingDays = 20;
+const fullTime = 2;
+const partTimeHrs = 4;
+const fullTimeHours = 8;
+const wagePerHour = 20;
+const workingDays = 20;
+const maxHrsMonth= 160;
 
 function getEmpWorkingHrs(empCheck) {
     switch(empCheck) {
         case partTime:
-            return PartTimeHours;
+            return partTimeHrs;
     
-        case FullTime:
-            return FullTimeHours;
+        case fullTime:
+            return fullTimeHours;
     
             default:
                 return 0;
@@ -19,9 +20,12 @@ function getEmpWorkingHrs(empCheck) {
 }
 
 let totalEmpHrs = 0;
-for (day = 0; day < WorkingDays; day++) {
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getEmpWorkingHrs(empCheck);
+let totalWorkingDays = 0;
+while (totalEmpHrs <= maxHrsMonth &&
+        totalWorkingDays < workingDays) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            totalEmpHrs += getEmpWorkingHrs(empCheck);
 }
-let empWage = totalEmpHrs * WagePerHour;
-console.log("Employee Total Working Hours: "+totalEmpHrs +"   Employee wage: "+empWage);
+let empWage = totalEmpHrs * wagePerHour;
+console.log("Total Working Days: "+totalWorkingDays +" Employee Total Working Hours: "+totalEmpHrs +" Employee wage: "+empWage);
