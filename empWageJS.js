e009b9aff66c2c258192c1afc823d72c58eecdfb
KratiGupta <kratigupta8545@gmail.com>
2021-09-06 08:02:43 +0530

    Added program to calculate emp wage for parttime and fulltime

--- empWageJS.js
+++ empWageJS.js
@@ -1,8 +1,26 @@
-const empAbsent = 0
+const partTime = 1;
+const fullTime= 2;
+const partTimeHrs = 4;
+const fullTimeHrs = 8;
+const WagePerHour= 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == empAbsent) {
-    console.log("Employee is Absent.");
-} else {
-    console.log("Employee is Present.");
+let empHrs = 0;
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck) {
+    case partTime:
+        empHrs = partTimeHrs;
+        console.log("Employee is Part time Present")
+        break;
+    
+    case fullTime:
+        empHrs = fullTimeHrs;
+        console.log("Employee is fulltime present")
+        break;
+    
+        default:
+            empHrs = 0;
+            console.log("Employee is absent")
 }
+let empWage = empHrs * WagePerHour;
+console.log("Employee wage: "+empWage);
